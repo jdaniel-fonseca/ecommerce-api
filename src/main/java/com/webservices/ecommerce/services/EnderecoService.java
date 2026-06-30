e3686b0266cb7272c795895bb6542286419f6ce4
jdaniel-fonseca <jose.jdfonseca@gmail.com>
2026-06-30 20:58:18 -0300

    Criando a classe 'EnderecoService'

---
+++ src/main/java/com/webservices/ecommerce/services/EnderecoService.java
@@ -0,0 +1,77 @@
+package com.webservices.ecommerce.services;
+import com.webservices.ecommerce.dto.request.EnderecoRequestDTO;
+import com.webservices.ecommerce.dto.response.EnderecoResponseDTO;
+import com.webservices.ecommerce.entities.Endereco;
+import com.webservices.ecommerce.repositories.EnderecoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class EnderecoService {
+
+    private EnderecoRepository enderecoRepository;
+
+    public EnderecoService(EnderecoRepository enderecoRepository) {
+        this.enderecoRepository = enderecoRepository;
+    }
+
+    public List<EnderecoResponseDTO> findAll() {
+        List<Endereco> enderecos = enderecoRepository.findAll();
+        List<EnderecoResponseDTO> enderecosResponseDTO = new ArrayList<>();
+
+        for(Endereco endereco : enderecos) {
+            enderecosResponseDTO.add(responseDtoConverter(endereco));
+        }
+        return enderecosResponseDTO;
+    }
+
+    public EnderecoResponseDTO findById(Long id) {
+        Endereco endereco = this.enderecoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Endereço não encontrado."));
+
+        return new EnderecoResponseDTO(endereco);
+    }
+
+    public EnderecoResponseDTO save(EnderecoRequestDTO enderecoRequestDTO) {
+        Endereco endereco = requestDtoConverter(enderecoRequestDTO);
+        Endereco enderecoSalvo = enderecoRepository.save(endereco);
+        return responseDtoConverter(enderecoSalvo);
+    }
+
+    public void deleteById(Long id) {
+        this.enderecoRepository.deleteById(id);
+    }
+
+    public EnderecoResponseDTO update(EnderecoRequestDTO enderecoRequestDTO, Long id){
+        Endereco endereco = enderecoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Endereço não encontrado"));
+        updateData(enderecoRequestDTO, endereco);
+        Endereco enderecoSalvo = enderecoRepository.save(endereco);
+        return responseDtoConverter(enderecoSalvo);
+    }
+
+    private void updateData(EnderecoRequestDTO enderecoRequestDTO, Endereco endereco) {
+        endereco.setCep(enderecoRequestDTO.getCep());
+        endereco.setNumero(enderecoRequestDTO.getNumero());
+        endereco.setRua(enderecoRequestDTO.getRua());
+        endereco.setCidade(enderecoRequestDTO.getCidade());
+        endereco.setEstado(enderecoRequestDTO.getEstado());
+    }
+
+    private EnderecoResponseDTO responseDtoConverter(Endereco endereco) {
+        return new EnderecoResponseDTO(endereco);
+    }
+
+    private Endereco requestDtoConverter(EnderecoRequestDTO enderecoRequestDTO) {
+        Endereco endereco = new Endereco();
+        endereco.setCep(enderecoRequestDTO.getCep());
+        endereco.setRua(enderecoRequestDTO.getRua());
+        endereco.setNumero(enderecoRequestDTO.getNumero());
+        endereco.setCidade(enderecoRequestDTO.getCidade());
+        endereco.setEstado(enderecoRequestDTO.getEstado());
+        return endereco;
+    }
+}
