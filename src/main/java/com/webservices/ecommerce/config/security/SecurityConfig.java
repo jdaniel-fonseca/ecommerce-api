a256d41dda7ded141c812e909685a8d907c62815
jdaniel-fonseca <jose.jdfonseca@gmail.com>
2026-07-13 22:05:54 -0300

    implmentando o fluxo de autenticação

--- src/main/java/com/webservices/ecommerce/config/security/SecurityConfig.java
+++ src/main/java/com/webservices/ecommerce/config/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +31,10 @@ public class SecurityConfig {
 
         //esse metodo serve para que o Spring "entenda" como fazer a DI na classe "AutenticacaoController", por isso o @Bean
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
